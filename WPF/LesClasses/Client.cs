40eee9fafb62e8274c3dc7f8b382c68d38dbc446
mattéo bertauld-bécourt <matteo.bertauld-becourt@etu.univ-smb.fr>
2024-06-04 10:12:18 +0200

    Fusion de toutes les fenetres. Integration client à la base.

--- WPF/LesClasses/Client.cs
+++ WPF/LesClasses/Client.cs
@@ -1,35 +1,167 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WPF;
 
 namespace SAE2._01
 {
     public class Client
     {
+        private static int staticAutoIncrementNumClient;
+
         private int numClient;
         private string nomClient;
         private string adresseRueClient;
         private string adresseCpClient;
         private string adresseVilleClient;
+        private string telephone;
         private string mail;
 
-        public int NumClient { get => numClient; set => numClient = value; }
-        public string NomClient { get => nomClient; set => nomClient = value; }
-        public string AdresseRueClient { get => adresseRueClient; set => adresseRueClient = value; }
-        public string AdresseCpClient { get => adresseCpClient; set => adresseCpClient = value; }
-        public string AdresseVilleClient { get => adresseVilleClient; set => adresseVilleClient = value; }
-        public string Mail { get => mail; set => mail = value; }
-
-        public Client(int numClient, string nomClient, string adresseRueClient, string adresseCpClient, string adresseVilleClient, string mail)
+        public int NumClient
         {
-            NumClient = numClient;
-            NomClient = nomClient;
-            AdresseRueClient = adresseRueClient;
-            AdresseCpClient = adresseCpClient;
-            AdresseVilleClient = adresseVilleClient;
-            Mail = mail;
+            get
+            {
+                return numClient;
+            }
+
+            set
+            {
+                numClient = value;
+            }
         }
+
+        public string NomClient
+        {
+            get
+            {
+                return nomClient;
+            }
+
+            set
+            {
+                nomClient = value;
+            }
+        }
+
+        public string AdresseRueClient
+        {
+            get
+            {
+                return adresseRueClient;
+            }
+
+            set
+            {
+                adresseRueClient = value;
+            }
+        }
+
+        public string AdresseCpClient
+        {
+            get
+            {
+                return adresseCpClient;
+            }
+
+            set
+            {
+                adresseCpClient = value;
+            }
+        }
+
+        public string AdresseVilleClient
+        {
+            get
+            {
+                return adresseVilleClient;
+            }
+
+            set
+            {
+                adresseVilleClient = value;
+            }
+        }
+
+        public string Mail
+        {
+            get
+            {
+                return this.mail;
+            }
+
+            set
+            {
+                this.mail = value;
+            }
+        }
+
+        public static int StaticAutoIncrementNumClient
+        {
+            get
+            {
+                staticAutoIncrementNumClient += 1;
+                return staticAutoIncrementNumClient;
+            }
+        }
+
+        public string Telephone
+        {
+            get
+            {
+                return this.telephone;
+            }
+
+            set
+            {
+                this.telephone = value;
+            }
+        }
+
+        public Client(string nomClient, string adresseRueClient, string adresseCpClient, string adresseVilleClient,string telephone, string mail)
+        {
+            this.NumClient = StaticAutoIncrementNumClient;
+            this.NomClient = nomClient;
+            this.AdresseRueClient = adresseRueClient;
+            this.AdresseCpClient = adresseCpClient;
+            this.AdresseVilleClient = adresseVilleClient;
+            this.Telephone = telephone;
+            this.Mail = mail;
+        }
+
+
+        public int Create()
+        {
+            String sql = $"insert into client (num_client,nom_client,adresse_rue_client,adresse_cp_client,adresse_ville_client,telephone_client,mail_client) values (" +
+            $"'{this.NumClient}','{this.NomClient}'," +
+            $"'{this.AdresseRueClient}','{this.AdresseCpClient}','{this.AdresseVilleClient}'," +
+            $"{this.Telephone}','{this.Mail}');";
+
+            return DataAccess.Instance.SetData(sql);
+        }
+        public static ObservableCollection<Client> Read()
+        {
+            ObservableCollection<Client> lesClients = new ObservableCollection<Client>();
+            String sql = "SELECT * FROM client";
+            DataTable dt = DataAccess.Instance.GetData(sql);
+            
+            foreach (DataRow res in dt.Rows)
+            {
+                Client nouveau = new Client(
+                    res["nom_client"].ToString(),
+                    res["adresse_rue_client"].ToString(),
+                    res["adresse_cp_client"].ToString(),
+                    res["adresse_ville_client"].ToString(),
+                    res["telephone_client"].ToString(),
+                    res["mail_client"].ToString()
+                    );
+                lesClients.Add(nouveau);
+            }
+            return lesClients;
+        }
+
     }
 }
---
+++ WPF/MainWindow.xaml.cs
@@ -0,0 +1,204 @@
+﻿using SAE2._01;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool Filtre = false;
+        bool clientSelectionner = true;
+        bool creerNouveauClient;
+        ObservableCollection<Client> ListeClients = new ObservableCollection<Client>();
+
+
+        public bool ClientSelectionner
+        {
+            get
+            {
+                return this.clientSelectionner;
+            }
+
+            set
+            {
+                this.clientSelectionner = value;
+            }
+        }
+
+        public bool CreerNouveauClient
+        {
+            get
+            {
+                return this.creerNouveauClient;
+            }
+
+            set
+            {
+                this.creerNouveauClient = value;
+            }
+        }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            SeConnecter fenetreConnexion = new SeConnecter();
+            fenetreConnexion.ShowDialog();
+            if (fenetreConnexion.DialogResult == false)
+            {
+                System.Windows.Application.Current.Shutdown();
+            }
+        }
+
+        // Fenetre MenuEmploye
+
+        private void ButtonVisualiserReservation_Click(object sender, RoutedEventArgs e)
+        {
+            GridMenuEmploye.Visibility = Visibility.Hidden;
+            GridVisualiserReservation.Visibility = Visibility.Visible;
+        }
+
+        private void ButtonEffectuerReservation_Click(object sender, RoutedEventArgs e)
+        {
+            InitialiserFenetreFicheClient();
+            GridMenuEmploye.Visibility = Visibility.Hidden;
+            GridFicheClient.Visibility = Visibility.Visible;
+        }
+
+        // Fenetre Fiche CLient
+
+        private void InitialiserFenetreFicheClient()
+        {
+            ListeClients = Client.Read();
+            ComboBoxSelectionClient.Items.Clear();
+            foreach (Client c in ListeClients)
+            {
+                ComboBoxSelectionClient.Items.Add(c);
+            }
+        }
+
+        private void ButtonNouveauClient_Checked(object sender, RoutedEventArgs e)
+        {
+            TextBoxNom.Visibility = Visibility.Visible;
+            TextBoxPrenom.Visibility = Visibility.Visible;
+            TextBoxAdresse.Visibility = Visibility.Visible;
+            TextBoxVille.Visibility = Visibility.Visible;
+            TextBoxCodePostal.Visibility = Visibility.Visible;
+            TextBoxPays.Visibility = Visibility.Visible;
+            TextBoxTelephone.Visibility = Visibility.Visible;
+            TextBoxMail.Visibility = Visibility.Visible;
+            ButtonEntreprise.Visibility = Visibility.Visible;
+
+            ComboBoxSelectionClient.Visibility = Visibility.Hidden;
+            LabelOU.Visibility = Visibility.Hidden;
+        }
+
+        private void ButtonValiderFicheClient_Click(object sender, RoutedEventArgs e)
+        {
+            if (CreerNouveauClient)
+            {
+                //Client NouveauClient = 
+                ClientSelectionner = true;
+            }
+
+            if (ClientSelectionner)
+            {
+                GridReservationVehicule.Visibility = Visibility.Visible;
+                GridFicheClient.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                Console.WriteLine("Erreur : Aucun cliet n'est séléctionner");
+            }
+        }
+
+
+        // Choix des Vehicules
+
+
+        private void ButtonMagasin_Checked(object sender, RoutedEventArgs e)
+        {
+            ComboBoxMagasin.Visibility = Visibility.Visible;
+            CalendarReservation.Visibility = Visibility.Hidden;
+            ComboBoxCategorieVehicule.Visibility = Visibility.Hidden;
+        }
+
+        private void ButtonCategorieVehicule_Checked(object sender, RoutedEventArgs e)
+        {
+            ComboBoxCategorieVehicule.Visibility = Visibility.Visible;
+            CalendarReservation.Visibility = Visibility.Hidden;
+            ComboBoxMagasin.Visibility = Visibility.Hidden;
+        }
+
+        private void ButtonDateRéservation_Checked(object sender, RoutedEventArgs e)
+        {
+            CalendarReservation.Visibility = Visibility.Visible;
+            ComboBoxMagasin.Visibility = Visibility.Hidden;
+            ComboBoxCategorieVehicule.Visibility = Visibility.Hidden;
+        }
+
+        private void ButtonFiltre_Click(object sender, RoutedEventArgs e)
+        {
+            if (Filtre) { ButtonFiltre.Content = "Filtre : Automatique"; }
+            else { ButtonFiltre.Content = "Filtre : Manuelle"; }
+            Filtre = !Filtre;
+        }
+
+        private void ButtonAjouterVehicule_Click(object sender, RoutedEventArgs e)
+        {
+            ListeBoxVehiculeChoisit.Items.Add("Véhicule");
+        }
+
+
+        private void Valider_Critère_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+            ListeBoxVehiculeRecherche.Items.Clear();
+            ListeBoxVehiculeRecherche.Items.Add("Véhicule 1");
+            ListeBoxVehiculeRecherche.Items.Add("Véhicule 2");
+            ListeBoxVehiculeRecherche.Items.Add("Véhicule 3");
+            */
+        }
+
+        private void ButtonSuivant_Click(object sender, RoutedEventArgs e)
+        {
+            GridReservationVehicule.Visibility = Visibility.Hidden;
+            GridValidationReservation.Visibility = Visibility.Visible;
+        }
+
+
+        //  Fenetre Validation reservation
+
+
+        private void ButtonValider_Click(object sender, RoutedEventArgs e)
+        {
+            GridValidationReservation.Visibility = Visibility.Hidden;
+            GridMenuEmploye.Visibility = Visibility.Visible;
+        }
+
+
+        // Fenetre Visualiser Reservation
+
+
+        private void ButtonRetour_Click(object sender, RoutedEventArgs e)
+        {
+            GridVisualiserReservation.Visibility = Visibility.Hidden;
+            GridMenuEmploye.Visibility = Visibility.Visible;
+        }
+    }
+}
